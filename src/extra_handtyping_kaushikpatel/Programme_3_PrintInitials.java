1d54023780a71ce3383250683bb28230b777a074
PrimeKP <kaushik.pchase@gmail.com>
2022-10-13 21:12:19 +0100

    JD week 1 writing homework kaushik patel

---
+++ src/extra_handtyping_kaushikpatel/Programme_3_PrintInitials.java
@@ -0,0 +1,22 @@
+package extra_handtyping_kaushikpatel;
+
+/*
+    3. Write a program that will print your in letter that
+    are nine line tall. Each big letter should be made up of a bunch of *'s. For
+    example, if your initials were "JAVA", then the output would look something
+    like:
+ */
+public class Programme_3_PrintInitials {
+    public static void main (String [] args) {
+        System.out.println("*********  ********  *********  ********");
+        System.out.println("      **   **    **        **   **    **");
+        System.out.println("      **   **    **        **   **    **");
+        System.out.println("      **   **    **        **   **    **");
+        System.out.println("      **   ********        **   ********");
+        System.out.println("  **  **   **    **    **  **   **    **");
+        System.out.println("  **  **   **    **    **  **   **    **");
+        System.out.println("   ****    **    **     ****    **    **");
+        System.out.println("    **     **    **      **     **    **");
+    }
+
+}
